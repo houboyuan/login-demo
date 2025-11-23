f457b5487dd9f42a178ff9a4bb9953476e732975
houboyuan <167880515+houboyuan@users.noreply.github.com>
2025-11-23 20:16:09 +0800

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -5,4 +5,5 @@ load_dotenv()
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
-REDIRECT_URL = os.getenv("REDIRECT_URL", "https://yourusername.github.io/bytebase-login")
+REDIRECT_URL = os.getenv("REDIRECT_URL", "https://houboyuan.github.io/login-demo")
+
