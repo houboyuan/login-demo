6c5c8e754e16e1977c852da2b3f1a895da310ae0
houboyuan <167880515+houboyuan@users.noreply.github.com>
2025-11-23 21:01:21 +0800

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -6,4 +6,3 @@ load_dotenv()
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 REDIRECT_URL = os.getenv("REDIRECT_URL", "https://houboyuan.github.io/login-demo")
-
